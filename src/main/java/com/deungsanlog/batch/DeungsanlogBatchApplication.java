217994ebd48143ac5b7970c8f2df7c85c544a5fe
Pushseo <pushseo.kim@gmail.com>
2025-05-16 18:54:28 +0900

    🚀 initial

---
+++ src/main/java/com/deungsanlog/batch/DeungsanlogBatchApplication.java
@@ -0,0 +1,13 @@
+package com.deungsanlog.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeungsanlogBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeungsanlogBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deungsanlog/batch/DeungsanlogBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deungsanlog.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeungsanlogBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
