1f9e8be5ec4f23297bb0a621185e4e25286d3379
Pushseo <pushseo.kim@gmail.com>
2025-05-21 11:22:48 +0900

    feat: MountainSunInfo 기본 JPA 레포지토리 생성

---
+++ src/main/java/com/deungsanlog/batch/domain/mountain/repository/MountainSunInfoRepository.java
@@ -0,0 +1,7 @@
+package com.deungsanlog.batch.domain.mountain.repository;
+
+import com.deungsanlog.batch.domain.mountain.entity.MountainSunInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MountainSunInfoRepository extends JpaRepository<MountainSunInfo, Long> {
+}
